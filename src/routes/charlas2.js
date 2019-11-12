bc3f490aba08fc3ca6eba996deeeca26131d5503
Ismael Leon <ismael@advansys.local>
2019-11-12 18:00:50 -0400

    sequelize charla

---
+++ src/routes/charlas2.js
@@ -0,0 +1,160 @@
+import express from 'express'
+import Sequelize from 'Sequelize'
+
+var routerCharla2 = express.Router();
+
+let respuesta = {
+    error: false,
+    codigo: 200,
+    mensaje: ''
+};
+
+var sequelize = new Sequelize('evento_tech_day', 'root', 'yadiel', {
+    host: 'localhost',
+    dialect: 'mysql',
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    },
+  });
+
+  const charla = sequelize.define('charlas', {
+    // attributes
+    nombre: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    hora: {
+      type: Sequelize.STRING,
+      allowNull: false 
+    },
+    expositor_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: 'expositors', // <<< Note, its table's name, not object name
+        referencesKey: 'id' // <<< Note, its a column name        
+    },
+      tags: {
+          type: Sequelize.STRING,
+          allowNull: false 
+        }
+}, {
+    timestamps: false
+  });
+
+
+  // Lista de charlas
+  routerCharla2.get('/', (req, res) => {
+    charla.findAll().then(charlas => {
+        res.setHeader('Content-Type', 'application/json')
+        res.send(JSON.stringify(charlas)); 
+        //console.log("All users:", JSON.stringify(expositores,null,4));
+    })
+    .catch(error => {
+        //console.error('Error:', error)
+        res.setHeader('Content-Type', 'application/json')
+        res.send(JSON.stringify(error))   
+    }) ;
+})
+
+// charlas por Id
+routerCharla2.get('/:id', (req, res) => { 
+    var idexpo = req.params.id
+    charla.findAll({
+        where: {
+          id: idexpo
+        }
+      }).then(charlas => {
+        res.setHeader('Content-Type', 'application/json')
+        res.send(JSON.stringify(charlas)); 
+        //console.log("All users:", JSON.stringify(expositores,null,4));
+    })
+    .catch(error => {
+        //console.error('Error:', error)
+        res.setHeader('Content-Type', 'application/json')
+        res.send(JSON.stringify(error))   
+    }) ;
+})
+
+
+// Registrar Charla
+routerCharla2.post('/', (req, res) => {
+
+    if(!req.body.nombre || !req.body.hora || req.body.expositor_id==0) {
+        respuesta = {
+         error: true,
+         codigo: 502,
+         mensaje: 'El campo nombre, hora y expositor son requeridos'
+        };
+       }
+
+    var obj_charla  = {
+        nombre: req.body.nombre,
+        tags: req.body.tags,
+        expositor_id: req.body.expositor_id,
+        hora: req.body.hora
+    }
+
+    charla.create(obj_charla).then(exp => {
+        respuesta = {
+            error: false,
+            codigo: 200,
+            mensaje: "Registro creado!. Id " + exp.id
+           };
+           console.log(respuesta)
+        res.send(respuesta)
+      })
+      .catch(error => {
+        res.setHeader('Content-Type', 'application/json')
+        res.send(JSON.stringify(error))   
+     });
+
+})
+
+// Actualizar Expositor
+routerCharla2.put('/:id', (req, res) => {
+    var id = req.params.id
+    var cuerpo = req.body
+
+    if(!cuerpo.nombre || !cuerpo.hora || !cuerpo.tags || cuerpo.expositor_id==0) {
+        respuesta = {
+         error: true,
+         codigo: 502,
+         mensaje: 'El campo nombre, hora, tags y expositor son requeridos'
+        };
+       }
+
+    var obj_charla = {
+        nombre: cuerpo.nombre,
+        hora: cuerpo.hora,
+        tags: cuerpo.tags,
+        expositor_id: cuerpo.expositor_id
+    }
+
+    
+    charla.update(obj_charla, {
+        where: {
+          id: id
+        }
+      }).then(() => {
+
+        respuesta = {
+            error: false,
+            codigo: 200,
+            mensaje: "Registro Actualizado"
+           };
+        res.send(respuesta)
+
+    })
+      .catch(error => {
+        res.setHeader('Content-Type', 'application/json')
+        res.send(JSON.stringify(error))   
+     });
+
+})  
+
+
+
+
+export default routerCharla2;
