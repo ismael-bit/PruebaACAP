d529408a7a93278fc735de296ebf4108ca1f65b3
ismael-bit <55302448+ismael-bit@users.noreply.github.com>
2019-11-13 18:11:39 -0400

    fixes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import routes from './routes'
-import cors from 'cors'
-const app = express()
-const port = 3000
+import express from "express";
+import bodyParser from "body-parser";
+import routes from "./routes";
+import cors from "cors";
+const app = express();
+const port = 3000;
 
 // middleware
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use(cors({
-    origin: '*'
-  }))
+app.use(
+  cors({
+    origin: "*"
+  })
+);
 
-  app.use('/api', routes)
+app.use("/api", routes);
 
-app.get('/', (req, res) => {
- res.send('Hola Mundo....!')
-})
+app.get("/", (req, res) => {
+  res.send("Hola Mundo....!");
+});
 
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- src/routes/expositor2.js
+++ src/routes/expositor2.js
@@ -1,25 +1,27 @@
-import express from 'express'
-import Sequelize from 'Sequelize'
+import express from "express";
+import Sequelize from "Sequelize";
 
 var routerExpositor2 = express.Router();
 
 let respuesta = {
-    error: false,
-    codigo: 200,
-    mensaje: ''
+  error: false,
+  codigo: 200,
+  mensaje: ""
 };
 
-var sequelize = new Sequelize('evento_tech_day', 'root', 'yadiel', {
-    host: 'localhost',
-    dialect: 'mysql',
-    pool: {
-      max: 5,
-      min: 0,
-      idle: 10000
-    },
-  });
+var sequelize = new Sequelize("evento_tech_day", "root", "yadiel", {
+  host: "localhost",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
 
-  const expositor = sequelize.define('expositor', {
+const expositor = sequelize.define(
+  "expositor",
+  {
     // attributes
     nombre: {
       type: Sequelize.STRING,
@@ -27,154 +29,156 @@ var sequelize = new Sequelize('evento_tech_day', 'root', 'yadiel', {
     },
     cuenta_github: {
       type: Sequelize.STRING,
-      allowNull: false 
+      allowNull: false
     },
     correo: {
-        type: Sequelize.STRING,
-        allowNull: false 
-      }
-}, {
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+  },
+  {
     timestamps: false
-  });
+  }
+);
 
-
-  // Lista de expositores
-routerExpositor2.get('/', (req, res) => {
-    expositor.findAll().then(expositores => {
-        res.setHeader('Content-Type', 'application/json')
-        res.send(JSON.stringify(expositores)); 
-        //console.log("All users:", JSON.stringify(expositores,null,4));
+// Lista de expositores
+routerExpositor2.get("/", (req, res) => {
+  expositor
+    .findAll()
+    .then(expositores => {
+      res.setHeader("Content-Type", "application/json");
+      res.send(JSON.stringify(expositores));
+      //console.log("All users:", JSON.stringify(expositores,null,4));
     })
     .catch(error => {
-        //console.error('Error:', error)
-        res.setHeader('Content-Type', 'application/json')
-        res.send(JSON.stringify(error))   
-    }) ;
-})
+      //console.error('Error:', error)
+      res.setHeader("Content-Type", "application/json");
+      res.send(JSON.stringify(error));
+    });
+});
 
 // Expositores por Id
-routerExpositor2.get('/:id', (req, res) => { 
-    var idexpo = req.params.id
-    expositor.findOne({
-        where: {
-          id: idexpo
-        }
-      }).then(expositores => {
-        res.setHeader('Content-Type', 'application/json')
-        res.send(JSON.stringify(expositores)); 
-        //console.log("All users:", JSON.stringify(expositores,null,4));
+routerExpositor2.get("/:id", (req, res) => {
+  var idexpo = req.params.id;
+  expositor
+    .findOne({
+      where: {
+        id: idexpo
+      }
+    })
+    .then(expositores => {
+      res.setHeader("Content-Type", "application/json");
+      res.send(JSON.stringify(expositores));
+      //console.log("All users:", JSON.stringify(expositores,null,4));
     })
     .catch(error => {
-        //console.error('Error:', error)
-        res.setHeader('Content-Type', 'application/json')
-        res.send(JSON.stringify(error))   
-    }) ;
-})
-
+      //console.error('Error:', error)
+      res.setHeader("Content-Type", "application/json");
+      res.send(JSON.stringify(error));
+    });
+});
 
 // Registrar Expositor
-routerExpositor2.post('/', (req, res) => {
+routerExpositor2.post("/", (req, res) => {
+  if (!req.body.nombre || !req.body.correo || !req.body.cuenta_github) {
+    respuesta = {
+      error: true,
+      codigo: 502,
+      mensaje: "El campo nombre, correo y cuenta_github son requeridos"
+    };
+  }
 
-    if(!req.body.nombre || !req.body.correo || !req.body.cuenta_github) {
-        respuesta = {
-         error: true,
-         codigo: 502,
-         mensaje: 'El campo nombre, correo y cuenta_github son requeridos'
-        };
-       }
+  var obj_expositor = {
+    nombre: req.body.nombre,
+    correo: req.body.correo,
+    cuenta_github: req.body.cuenta_github
+  };
 
-    var obj_expositor = {
-        nombre: req.body.nombre,
-        correo: req.body.correo,
-        cuenta_github: req.body.cuenta_github
-    }
-
-    expositor.create(obj_expositor).then(exp => {
-        respuesta = {
-            error: false,
-            codigo: 200,
-            mensaje: "Registro creado!. Id " + exp.id
-           };
-           console.log(respuesta)
-        res.send(respuesta)
-      })
-      .catch(error => {
-        res.setHeader('Content-Type', 'application/json')
-        res.send(JSON.stringify(error))   
-     });
-
-})
+  expositor
+    .create(obj_expositor)
+    .then(exp => {
+      respuesta = {
+        error: false,
+        codigo: 200,
+        mensaje: "Registro creado!. Id " + exp.id
+      };
+      console.log(respuesta);
+      res.send(respuesta);
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.send(JSON.stringify(error));
+    });
+});
 
 // Actualizar Expositor
-routerExpositor2.put('/:id', (req, res) => {
-    var id = req.params.id
-    var cuerpo = req.body
+routerExpositor2.put("/:id", (req, res) => {
+  var id = req.params.id;
+  var cuerpo = req.body;
 
-    if(!cuerpo.nombre || !cuerpo.correo || !cuerpo.cuenta_github) {
-      respuesta = {
-       error: true,
-       codigo: 502,
-       mensaje: 'El campo nombre, correo y cuenta_github son requeridos'
-      };
-     }
-
-    var obj_expositor = {
-        nombre: cuerpo.nombre,
-        correo: cuerpo.correo,
-        cuenta_github: cuerpo.cuenta_github
-    }
-
-    expositor.update(obj_expositor, {
-        where: {
-          id: id
-        }
-      }).then(() => {
-
-        respuesta = {
-            error: false,
-            codigo: 200,
-            mensaje: "Registro Actualizado"
-           };
-        res.send(respuesta)
-
-    })
-      .catch(error => {
-        res.setHeader('Content-Type', 'application/json')
-        res.send(JSON.stringify(error))   
-     });
-
-})
-
-// Elimiar Expositor
-routerExpositor2.delete('/:id', (req, res) => {
-  var id = req.params.id
-  if(id === 0) {
+  if (!cuerpo.nombre || !cuerpo.correo || !cuerpo.cuenta_github) {
     respuesta = {
-     error: true,
-     codigo: 502,
-     mensaje: 'El id debe ser mayor que cero'
+      error: true,
+      codigo: 502,
+      mensaje: "El campo nombre, correo y cuenta_github son requeridos"
     };
-   }
+  }
 
-  expositor.destroy( {
+  var obj_expositor = {
+    nombre: cuerpo.nombre,
+    correo: cuerpo.correo,
+    cuenta_github: cuerpo.cuenta_github
+  };
+
+  expositor
+    .update(obj_expositor, {
       where: {
         id: id
       }
-    }).then(() => {
-
+    })
+    .then(() => {
       respuesta = {
-          error: false,
-          codigo: 200,
-          mensaje: "Registro Eliminado"
-         };
-      res.send(respuesta)
-
-  })
+        error: false,
+        codigo: 200,
+        mensaje: "Registro Actualizado"
+      };
+      res.send(respuesta);
+    })
     .catch(error => {
-      res.setHeader('Content-Type', 'application/json')
-      res.send(JSON.stringify(error))   
-   });
+      res.setHeader("Content-Type", "application/json");
+      res.send(JSON.stringify(error));
+    });
+});
 
-})
+// Elimiar Expositor
+routerExpositor2.delete("/:id", (req, res) => {
+  var id = req.params.id;
+  if (id === 0) {
+    respuesta = {
+      error: true,
+      codigo: 502,
+      mensaje: "El id debe ser mayor que cero"
+    };
+  }
+
+  expositor
+    .destroy({
+      where: {
+        id: id
+      }
+    })
+    .then(() => {
+      respuesta = {
+        error: false,
+        codigo: 200,
+        mensaje: "Registro Eliminado"
+      };
+      res.send(respuesta);
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.send(JSON.stringify(error));
+    });
+});
 
 export default routerExpositor2;
